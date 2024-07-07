37ab6ed2a6656932b549b12c3c1161c62080a31a
djoe <djoe@djoe.djoe>
2024-07-07 15:31:37 +0530

    Changed to sqlite3 for educational purpose

--- app/FakeNewsDetectorAPI/FakeNewsDetectorAPI/settings.py
+++ app/FakeNewsDetectorAPI/FakeNewsDetectorAPI/settings.py
@@ -84,15 +84,10 @@ WSGI_APPLICATION = 'FakeNewsDetectorAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'fake_news_detector',
-        'USER': 'ai',
-        'PASSWORD': 'supersecpass',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
